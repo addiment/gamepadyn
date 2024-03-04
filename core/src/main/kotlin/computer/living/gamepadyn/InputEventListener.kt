4228f315b36de5d3c3440cd1b41eabed5248110a
addiment <63932310+addiment@users.noreply.github.com>
2024-03-04 11:01:17 -0500

    Goodbye, Java!

---
+++ core/src/main/kotlin/computer/living/gamepadyn/InputEventListener.kt
@@ -0,0 +1,23 @@
+package computer.living.gamepadyn
+
+import computer.living.gamepadyn.Event.EventData
+
+/**
+ * Functional interface for Gamepadyn event listeners. See [Event]
+ * @see InputEventListener.onStateChange
+ */
+@FunctionalInterface
+fun interface InputEventListener<T : InputData, TD, TA, TAA>
+        where TD : Enum<TD>,
+              TD : ActionEnumDigital,
+              TA : Enum<TA>,
+              TA : ActionEnumAnalog1,
+              TAA : Enum<TAA>,
+              TAA : ActionEnumAnalog2
+{
+    /**
+     * Called when an action's state changes.
+     * @param ev The relevant event data.
+     */
+    fun onStateChange(ev: EventData<T, TD, TA, TAA>?)
+}
